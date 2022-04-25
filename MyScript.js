c0352198f3976fe8f4c203ace58e5d6a6837c747
Tolga Özkaya <tolgaozkaya.2002@gmail.com>
2022-04-25 20:40:07 +0300

    update 3

--- MyScript.js
+++ MyScript.js
@@ -1,5 +1,5 @@
 var loadTime = document.createElement("p");
-loadTime.textContent = 'You loaded this page on: ' + new Date() ;
+loadTime.textContent = 'You loaded this page on: ' + new Date();
 loadTime.style.color = 'white';
 document.getElementById("myDIV").appendChild(loadTime);
 
