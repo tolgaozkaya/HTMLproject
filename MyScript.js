fd6237e8308c85a749e2c8b9eea99ea5e06c281d
Tolga Özkaya <tolgaozkaya.2002@gmail.com>
2022-07-02 07:43:50 +0300

    initial commit

--- MyScript.js
+++ MyScript.js
@@ -1,4 +1,14 @@
-var loadTime = document.createElement("p");
-loadTime.textContent = 'You loaded this page on: ' + new Date();
-loadTime.style.color = 'white';
-document.getElementById("myDIV").appendChild(loadTime);
+$(document).ready(function () {
+    var tmpAnimation = 0; $("#logoID").click(function () {
+        var element = $("#logoID");
+        tmpAnimation = tmpAnimation + 360;
+        $({ degrees: tmpAnimation - 360 }).animate({ degrees: tmpAnimation }, {
+            duration: 1390,
+            step: function (now) {
+                element.css({
+                    transform: 'rotate(' + now + 'deg)'
+                });
+            }
+        });
+    });
+});
